ccc151d04ff63f36925774c19e69ffef90b456ac
Nicola Fargus <NikkiFargus@192.168.1.105>
2017-05-18 22:03:53 +0100

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,19 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards [0];
-var cardTwo = cards [2];
 
-cardsInPlay.push (cardOne);
-cardsInPlay.push (cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if  (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 	} else { 
 		alert("Sorry, try again.");
-	}
-}
+		}
+};
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]) 
+if (cardsInPlay.length === 2) 
+ checkForMatch();
+};
 
+flipCard(0)
+flipCard(2)
